0bda9bc07b1205fb31358d9e1c35ebef2f365de4
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-03-17 13:53:29 -0500

    control ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,17 +22,55 @@ class AlienInvasion:
     def run_game(self):
         """Start main game loop"""
         while True:
-            #Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             
-            #Redraw the screen
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            self._check_keydown_events(event)
+            self._checkkeyup_events(event)
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _check_keydown_events(self, event):
+        """respond to keypresses"""
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_UP:
+                self.ship.moving_up = True
+            elif event.key == pygame.K_DOWN:
+                self.ship.moving_down = True
+            elif event.key == pygame.K_q:
+                sys.exit()
+
+    def _check_keyup_events(self, event):
+        """respond to keyups"""
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
+            elif event.key == pygame.K_UP:
+                self.ship.moving_up = False
+            elif event.key == pygame.K_DOWN:
+                self.ship.moving_down = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to new screen"""
+        #Redraw the screen
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     #make a game instance and run game
