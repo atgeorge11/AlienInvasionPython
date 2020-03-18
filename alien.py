98b2a739688beffb4497ae8508e3acf829bb2dc5
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-03-17 19:28:24 -0500

    finished lose state

--- alien.py
+++ alien.py
@@ -29,7 +29,5 @@ class Alien(Sprite):
     def check_edges(self):
         """Return True if alien is at the edge of screen"""
         screen_rect = self.screen.get_rect()
-        print("Checking edges")
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
-            print("Changing directions")
             return True
